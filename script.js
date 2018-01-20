23f0a5d3707aed35fd583b3ede5f5c504d5752a8
Louistwee <27563848+Louistwee@users.noreply.github.com>
2018-01-20 20:11:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var page = {
-  title: 'test',
+  title: 'Example',
   items: [
     {
       type: 'menu',
@@ -7,12 +7,28 @@ var page = {
       items: [
         {
           type: 'item',
-          label: 'link1',
+          label: 'Item 1',
           url: '#',
           action:function(){alert('hoi')},
         },
+        {
+          type: 'search',
+          label: 'Zoeken...',
+          action: function(data){
+            setTimeout(function(){
+              data.stopLoading();
+            },2000);
+            setTimeout(function(){
+              var answ = prompt(data.value);
+            },1);
+          },
+        },
       ],
     },
+    {
+          type: 'text',
+          text: 'Hello welcome on the example page of *page.js*.<br> Try to use your _right mouse_ on severeal elements. <br>You can simply include icons ^car^ like this*^car^*',
+        },
   ],
 }
 $(function(){$.page(page);});
