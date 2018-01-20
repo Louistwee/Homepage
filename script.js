b9ad13df3cf25fd097c0687b786d58cd78e763c6
Louistwee <27563848+Louistwee@users.noreply.github.com>
2018-01-20 14:47:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
 var page = {
-  menu:{
-    items:[
-      {
-        type:'Home',
-        title:'Home',
-        url:'hompage.html',
-      },
-      {
-        type:'link',
-        title:'link1',
-        url:'#',
-      },
-      {
-        type:'link',
-        title:'link2',
-        url:'#',
-      },
-      {
-        type:'dropdown',
-        title:'dropdown',
-        url:'#',
-        items:[
-          {
-            type:'link',
-            title:'sub1',
-            url:'#',
-          },
-          {
-            type:'link',
-            title:'sub2',
-            url:'#',
-          }
-        ]
-      },
-      'right',
-      {
-        type:'search',
-        title:'search',
-        url:'#',
-      }
-    ],
-  }
+  title: '',
+  favicon: '',
+  items: [
+    {
+      type: 'Horizontal-navigation',
+      fixed: true,
+      items: [
+        {
+          type: 'Home',
+          title: 'Home',
+          url: 'hompage.html',
+        },
+        {
+          type: 'link',
+          title: 'link1',
+          url: '#',
+        },
+        {
+          type: 'link',
+          title: 'link2',
+          url: '#',
+        },
+        {
+          type: 'dropdown',
+          title: 'dropdown',
+          url: '#',
+          items: [
+            {
+              type: 'link',
+              title: 'sub1',
+              url: '#',
+            },
+            {
+              type: 'link',
+              title: 'sub2',
+              url: '#',
+            }
+          ]
+        },
+        'right',
+        {
+          type: 'search',
+          title: 'search',
+          url: '#',
+        },
+      ],
+    },
+  ],
 }
 $.page(page);
