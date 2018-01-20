8f29639119cb8e1f936cdb241553198d7a857846
Louistwee <27563848+Louistwee@users.noreply.github.com>
2018-01-20 13:26:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(function(){
+  $.feed('http://www.rssboard.org/files/sample-rss-2.xml',function(feed){
+    $('#feed-ticker').text(feed.items[0].title);
+  })
   
 })
+$.feed = function(URL,succes){
+  $.get(URL, function (data) {
+    var $data = $(data);
+    var result = {
+      title:$data.children('title').text(),
+      items:[],
+    };
+    $data.find("item").each(function () { // or "item" or whatever suits your feed
+      var $item = $(this);
+      result.items.push({
+        title:$item.children("title"),
+        author:$item.children("author"),
+        description:$item.children("description"),
+      })
+    });
+    succes(result);
+  });
+}
