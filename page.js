a8678dedf78c4f150f351ac02494e566019fff7a
Louistwee <27563848+Louistwee@users.noreply.github.com>
2018-01-20 14:52:17 +0100

    Update page.js

--- page.js
+++ page.js
@@ -1,5 +1,9 @@
-$.page = function(){
-  var page = $('<body></body>');
-  
-  $('html').append(page);
+$.page = function(page){
+  var body = $('body');
+  if(page.title){
+    $('head').append($('<title></title>').text(page.title))
+  }
+  for (i = 0; i < page.items.length; i++) {
+    body.append($.page[page.items[i]](page.items[i]));
+  } 
 }
