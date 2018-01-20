949af7bb571f6a64eeaf16698d974b78b326d8c8
Louistwee <27563848+Louistwee@users.noreply.github.com>
2018-01-20 15:09:13 +0100

    Update page.js

--- page.js
+++ page.js
@@ -1,9 +1,25 @@
-$.page = function(page){
+$.page = function(data){
   var body = $('body');
-  if(page.title){
-    $('head').append($('<title></title>').text(page.title))
+  if(data.title){
+    $('head').append($('<title></title>').text(data.title))
   }
-  for (i = 0; i < page.items.length; i++) {
-    body.append($.page[page.items[i]](page.items[i]));
-  } 
+  $.page.append(data.items,body);
+}
+$.page.append = function(itemlist,parent){
+  for (i = 0; i < itemlist.length; i++) {
+    let item;
+    $.extend(item,$.page[itemlist[i]].defaults,itemlist[i])
+    parent.append($.page[itemlist[i]](item));
+  }
+}
+$.page.menu = function(data){
+  var menu = $('<div class="ui menu"></div>');
+  $.page.append(data.items,menu);
+}
+$.page.item = function(data){
+  var item = $('<a class="item"></a>').text(data.label).attr('href',data.href);
+}
+$.page.item.defaults = {
+  label:'',
+  href:'#',
 }
